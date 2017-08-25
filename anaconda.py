729d78cfca3c8f4dbcd853e1dc305e85075f4ced
Robert Cavanaugh <pipe2figlet@gmail.com>
2017-08-25 16:56:43 -0700

    firrst commit

--- anaconda.py
+++ anaconda.py
@@ -1,23 +1,21 @@
 import os, subprocess
 
 def shell(cmd):
-    x = subprocess.check_output(cmd)
+    x = subprocess.check_output(cmd).decode()
     return x
 
-def by_lsb():
+def get_lsb():
     command = ['lsb_release', '-as']
     output = shell(command).splitlines()
-    print(output)
     return {
         'Distro': output[0],
         'Name': output[1],
         'Version': output[2],
         'Release': output[3]
     }
-
-def by_uname():
+def get_uname():
     command = ['uname', '-a']
-    output = str(shell(command)).split(' ')
+    output = shell(command).split(' ')
     return {
         'OS': output[0],
         'Hostname': output[1],
@@ -26,24 +24,24 @@ def by_uname():
         'Arch': output[-2:-1]
     }
 
-def by_whoami():
+def get_user():
     command = ['whoami']
     output = shell(command)
     return {'User': output}
 
 def anaconda():
-    lsb_info = by_lsb()
-    uname_info = by_uname()
-    distro = lsb_info['Distro'].decode().lower()
-    print(distro)
-    with open('templates/%s.txt' % distro) as f:
-        print('here')
+    lsb_info = get_lsb()
+    uname_info = get_uname()
+    distro = lsb_info['Distro'].lower()
+    ascii_file = 'templates/%s.txt' % distro
+    if not os.path.exists(ascii_file):
+        ascii_file = 'templates/unknown.txt'
+    with open(ascii_file) as f:
         ascii_art = f.read()
-        ascii_art = ascii_art.replace('%OS.NAME', lsb_info['Name'].decode())
-        ascii_art = ascii_art.replace('%OS.VERSION', lsb_info['Version'].decode())
-        ascii_art = ascii_art.replace('%OS.RELEASE', lsb_info['Release'].decode())
-
-
-
+        ascii_art = ascii_art.replace('%OS.NAME', lsb_info['Name'])
+        ascii_art = ascii_art.replace('%OS.VERSION', lsb_info['Version'])
+        ascii_art = ascii_art.replace('%OS.RELEASE', lsb_info['Release'])
+        ascii_art = ascii_art.replace('%USER', shell('whoami').strip())
+        ascii_art = ascii_art.replace('%HOST', uname_info['Hostname'])
         print(ascii_art)
 anaconda()
